e0e1fc105d493ad7f99dcaf48fdfe03f7ac768ae
Jim Koning <summonjikon@protonmail.com>
2021-03-16 11:34:17 +0100

    Enemy Movment

---
+++ Space Invaders/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float timer = 0; 
+    float timeToMove = 0.5f;
+    int numOfMovements = 0; 
+    float speed = 0.50f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > timeToMove && numOfMovements != 11)
+        {
+            transform.Translate(new Vector3(speed, 0, 0));    
+            timer = 0; 
+            numOfMovements++;
+        }
+        if(numOfMovements == 11)
+        {
+            transform.Translate(new Vector3(0, -1, 0)); 
+            numOfMovements = -1;
+            speed = -speed;
+        } 
+    }
+}
---
+++ Space Invaders/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject projectile;
+    public GameObject projectileClone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        shoot(); 
+    }
+
+    void movement()
+    {
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(-5 * Time.deltaTime,0,0));
+        }
+                if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(5 * Time.deltaTime,0,0));
+        }
+                if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, -5 * Time.deltaTime,0));
+        }
+                if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, 5 * Time.deltaTime,0));
+        }
+
+    }
+
+    void shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))  
+        // && projectileClone == null
+        {
+            projectileClone = Instantiate(projectile, new Vector3(player.transform.position.x, player.transform.position.y + 0.6f, 0), player.transform.rotation) as GameObject;
+        }
+    }
+}
