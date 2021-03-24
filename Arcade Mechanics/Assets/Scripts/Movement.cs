3d9d1a267195cae2a66bd5388a1fbe2aa287e829
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2021-03-24 15:00:35 +0100

    changed something

---
+++ Arcade Mechanics/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	private Transform player;
+	public float speed;
+	public float maxBound, minBound;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Transform> ();
+	}
+
+	//Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+
+		if (player.position.x < minBound && h < 0)
+			h = 0;
+		else if (player.position.x > maxBound && h > 0)
+			h = 0;
+
+		player.position += Vector3.right * h * speed;
+	}
+
+}
